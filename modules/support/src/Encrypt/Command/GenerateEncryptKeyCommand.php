b4f068f791a3c2e4499fcc8ac7e24c9ecb4bce43
takemo101 <takemo101@gmail.com>
2024-01-15 11:26:13 +0900

    Add method to write App_key to Dotenv file

--- modules/support/src/Encrypt/Command/GenerateEncryptKeyCommand.php
+++ modules/support/src/Encrypt/Command/GenerateEncryptKeyCommand.php
@@ -5,6 +5,8 @@ namespace CmsTool\Support\Encrypt\Command;
 use CmsTool\Support\Encrypt\EncryptCipher;
 use Symfony\Component\Console\Output\OutputInterface;
 use Takemo101\Chubby\Console\Command\Command;
+use Takemo101\Chubby\Filesystem\LocalFilesystem;
+use Takemo101\Chubby\Support\ApplicationPath;
 
 class GenerateEncryptKeyCommand extends Command
 {
@@ -22,18 +24,50 @@ class GenerateEncryptKeyCommand extends Command
      *
      * @param OutputInterface $output
      * @param EncryptCipher $cipher
+     * @param LocalFilesystem $filesystem
+     * @param ApplicationPath $path
      * @return integer
      */
     public function handle(
         OutputInterface $output,
         EncryptCipher $cipher,
+        LocalFilesystem $filesystem,
+        ApplicationPath $path,
     ) {
         $key = $cipher->generateKey();
 
         $base64key = base64_encode($key);
 
+        $this->putAppKey($base64key, $filesystem, $path);
+
         $output->writeln("<info>key = {$base64key}</info>");
 
         return self::SUCCESS;
+    }
+
+    /**
+     * Write App_key to Dotenv
+     *
+     * @param string $key
+     * @param LocalFilesystem $filesystem
+     * @param ApplicationPath $path
+     * @return void
+     */
+    public function putAppKey(
+        string $key,
+        LocalFilesystem $filesystem,
+        ApplicationPath $path,
+    ): void {
+        $dotEnvPath = $path->getBasePath('.env');
+
+        if (!$filesystem->exists($dotEnvPath)) {
+            return;
+        }
+
+        if ($dotEnv = $filesystem->read($dotEnvPath)) {
+            $dotEnv = preg_replace('/APP_KEY=.*$/m', "APP_KEY={$key}", $dotEnv);
+
+            $filesystem->write($dotEnvPath, $dotEnv);
+        }
     }
 }

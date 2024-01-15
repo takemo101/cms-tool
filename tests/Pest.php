ddbdba767c803ff154e15d5d054d084c7e3ca0c7
takemo101 <takemo101@gmail.com>
2024-01-15 18:22:37 +0900

    Add test cases for cache, session, and support modules

--- tests/Pest.php
+++ tests/Pest.php
@@ -1,5 +1,23 @@
 <?php
 
+use Tests\Cache\TestCase as CacheTestCase;
+use Tests\Session\TestCase as SessionTestCase;
+use Tests\Support\TestCase as SupportTestCase;
 use Tests\TestCase;
 
 uses(TestCase::class)->in('Feature');
+
+// for cache
+uses(CacheTestCase::class)->in('../modules/cache/tests/Cache');
+
+// for session
+uses(SessionTestCase::class)->in(
+    '../modules/session/tests/Csrf',
+    '../modules/session/tests/Middleware',
+);
+
+// for support
+uses(SupportTestCase::class)->in(
+    '../modules/support/tests/JsonAccess',
+    '../modules/support/tests/Encrypt',
+);

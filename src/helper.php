6385b9d7af0c5fb90175c67fa48657d2df1d75b7
takemo101 <takemo101@gmail.com>
2024-06-17 16:12:48 +0900

    chore: Add support for FlashToast in session configuration

--- src/helper.php
+++ src/helper.php
@@ -2,7 +2,10 @@
 
 use Slim\Interfaces\RouteParserInterface;
 use Takemo101\Chubby\Http\Renderer\RedirectRenderer;
+use Takemo101\Chubby\Http\Renderer\ResponseRenderer;
 use Takemo101\CmsTool\Support\RedirectRendererFactory;
+use Takemo101\CmsTool\Support\Toast\ToastRenderer;
+use Takemo101\CmsTool\Support\Toast\ToastStyle;
 
 if (!function_exists('route')) {
     /**
@@ -35,5 +38,42 @@ if (!function_exists('redirect')) {
         return $url
             ? $factory->url($url)
             : $factory;
+    }
+}
+
+if (!function_exists('e')) {
+    /**
+     * Encode special characters in a string.
+     *
+     * @param string $value
+     * @param bool $doubleEncode
+     * @return string
+     */
+    function e($value, $doubleEncode = true)
+    {
+        return htmlspecialchars($value, ENT_QUOTES | ENT_SUBSTITUTE, 'utf-8', $doubleEncode);
+    }
+}
+
+if (!function_exists('toast')) {
+    /**
+     * Create a toast renderer
+     *
+     * @template T of ResponseRenderer
+     *
+     * @param T $response
+     * @param string|null $message
+     * @return ToastRenderer<T>
+     */
+    function toast(
+        ResponseRenderer $response,
+        ToastStyle $style = ToastStyle::Success,
+        ?string $message = null,
+    ): ToastRenderer {
+        return new ToastRenderer(
+            response: $response,
+            style: $style,
+            message: $message,
+        );
     }
 }

959699651c7a44c7f18cba56f0050374aa3e5c01
takemo101 <takemo101@gmail.com>
2024-06-18 13:52:29 +0900

    chore: Refactor configuration files for improved readability and descriptive names

--- config/slim.php
+++ config/slim.php
@@ -11,17 +11,8 @@ return [
     // Base path
     'base_path' => env('BASE_PATH'),
 
-    // Specify a class that implements Slimfactory, a factor class that generates Slim
-    'factory' => DefaultSlimFactory::class,
-
-    // Specify a class that implements Slimconfigurer to perform setting processing before executing Slim
-    'configurer' => DefaultSlimConfigurer::class,
-
     // Error output settings
     'error' => [
-
-        // Slim error handling specified classes that implement Error HandlerInterface
-        'handler' => ErrorHandler::class,
 
         // ErrorMiddleware error display setting
         'setting' => [
--- config/vendor/view.php
+++ config/vendor/view.php
@@ -1,15 +1,12 @@
 <?php
 
-use CmsTool\View\Accessor\DefaultDataAccessInvoker;
 use CmsTool\View\Contract\Htmlable;
-use CmsTool\View\DefaultTemplateFinder;
 use CmsTool\View\Twig\Extension\ConfigExtension;
 use CmsTool\View\Twig\Extension\RequestExtension;
 use CmsTool\View\Twig\Extension\FiltersExtension;
 use CmsTool\View\Twig\Extension\FormExtension;
 use CmsTool\View\Twig\Extension\FunctionsExtension;
 use CmsTool\View\Twig\Extension\RouteExtension;
-use CmsTool\View\Twig\TwigTemplateRenderer;
 use Takemo101\Chubby\Contract\Renderable;
 use Takemo101\CmsTool\Support\Accessor\MicroCmsApiAccessor;
 use Takemo101\CmsTool\Support\Accessor\ServerRequestAccessor;
@@ -24,15 +21,6 @@ use Takemo101\CmsTool\Support\Twig\TranslationExtension;
 use Twig\Extension\StringLoaderExtension;
 
 return [
-
-    // TemplateFinder implementation class name
-    'finder' => DefaultTemplateFinder::class,
-
-    // TemplateRenderer implementation class name
-    'renderer' => TwigTemplateRenderer::class,
-
-    // DataAccessInvoker implementation class name
-    'invoker' => DefaultDataAccessInvoker::class,
 
     'locations' => [],
 

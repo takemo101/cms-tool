17a69f4e5d3f7c6527472a71b5d85f5e2a4917fc
takemo101 <takemo101@gmail.com>
2024-06-27 14:46:46 +0900

    feat: Add HtmxResponse class for handling HTMX-specific response headers

---
+++ src/Support/Htmx/HtmxResponse.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Support\Htmx;
+
+
+use App\Support\Shared\AbstractResponse;
+use Psr\Http\Message\ServerRequestInterface;
+
+/**
+ * HTMX Response (defines only necessary methods)
+ *
+ * ref: https://htmx.org/reference/#response_headers
+ */
+class HtmxResponse extends AbstractResponse
+{
+    /**
+     * Set HX-Retarget header
+     *
+     * @param string $selector
+     * @return static
+     */
+    public function setHxRetarget(string $selector): static
+    {
+        return $this->addHeader('HX-Retarget', $selector);
+    }
+
+    /**
+     * Set HX-Reswap header
+     *
+     * @param string $selector
+     * @param string $event
+     * @return static
+     */
+    public function setHxReswap(string $selector): static
+    {
+        return $this->addHeader('HX-Reswap', $selector);
+    }
+
+    /**
+     * Set header for client-side redirect using Htmx
+     *
+     * @param string $to
+     * @return static
+     */
+    public function setHxRedirect(string $to): static
+    {
+        return $this->addHeader('HX-Redirect', $to);
+    }
+
+    /**
+     * Create a response for client-side redirect using Htmx
+     *
+     * @return static
+     */
+    public static function redirect(string $to): static
+    {
+        return (new self())->setHxRedirect($to);
+    }
+
+    /**
+     * Create a response for client-side redirect back using Htmx
+     *
+     * @param ServerRequestInterface $request
+     * @return static
+     */
+    public static function back(ServerRequestInterface $request): static
+    {
+        return (new self())->setHxRedirect($request->getHeaderLine('referer'));
+    }
+}

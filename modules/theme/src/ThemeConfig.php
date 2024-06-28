bbebaa88e2cec8f0ea60025086cb27ce426b0f5e
takemo101 <takemo101@gmail.com>
2024-06-28 12:58:40 +0900

    feat: Add constants for theme meta filename, readme filenames, assets path, and templates path

--- modules/theme/src/ThemeConfig.php
+++ modules/theme/src/ThemeConfig.php
@@ -4,11 +4,26 @@ namespace CmsTool\Theme;
 
 final class ThemeConfig
 {
+    /**
+     * Filename for the meta information required by the theme
+     */
     public const MetaFilename = 'theme.json';
+
+    /**
+     * Readme file names that contain the description of the theme in Markdown
+     */
     public const ReadmeFilenames = [
         'readme.md',
         'readme',
     ];
+
+    /**
+     * Directory name to store asset files such as images required by the theme
+     */
     public const AssetsPath = 'assets';
+
+    /**
+     * Directory name to store template files required by the theme
+     */
     public const TemplatesPath = 'templates';
 }

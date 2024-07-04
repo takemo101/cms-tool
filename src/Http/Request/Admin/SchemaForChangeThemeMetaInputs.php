449c0ca3ffba1590c5a5c9a867c0b11b00a1a880
takemo101 <takemo101@gmail.com>
2024-07-04 20:37:09 +0900

    feat: Add validation constraints to ChangeThemeMetaInputs

--- src/Http/Request/Admin/SchemaForChangeThemeMetaInputs.php
+++ src/Http/Request/Admin/SchemaForChangeThemeMetaInputs.php
@@ -2,20 +2,27 @@
 
 namespace Takemo101\CmsTool\Http\Request\Admin;
 
+use CmsTool\Theme\Schema\SchemaSettingId;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\Validator\Constraints\Type;
 use Symfony\Component\Validator\Constraints\All;
 use Symfony\Component\Validator\Constraints\Choice;
 use Symfony\Component\Validator\Constraints\Collection;
 use Symfony\Component\Validator\Constraints\Count;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\Optional;
 use Symfony\Component\Validator\Constraints\Required;
+use Symfony\Component\Validator\Constraints\Regex;
 
 readonly class SchemaForChangeThemeMetaInputs
 {
     public function __construct(
         #[NotBlank]
+        #[Regex(pattern: SchemaSettingId::Regex)]
+        #[Length(max: 255)]
         public string $id = '',
         #[NotBlank]
+        #[Length(max: 255)]
         public string $title = '',
         #[All(constraints: [
             new Type(type: 'array'),
@@ -34,6 +41,12 @@ readonly class SchemaForChangeThemeMetaInputs
                             'textarea',
                             'text',
                         ])
+                    ]),
+                    'id' => new Optional([
+                        new Type(type: 'string'),
+                        new NotBlank(),
+                        new Regex(pattern: SchemaSettingId::Regex),
+                        new Length(max: 255),
                     ]),
                 ],
                 allowExtraFields: true,

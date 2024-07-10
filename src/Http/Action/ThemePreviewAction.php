f543b6549458540bdf26bc254ea98d4666cf68d6
takemo101 <takemo101@gmail.com>
2024-07-11 07:54:32 +0900

    Normalize theme preview action and dependencies

--- src/Http/Action/ThemePreviewAction.php
+++ src/Http/Action/ThemePreviewAction.php
@@ -2,30 +2,48 @@
 
 namespace Takemo101\CmsTool\Http\Action;
 
+use CmsTool\Theme\ActiveTheme;
 use CmsTool\View\Support\RouteParserDecorator;
 use Psr\Http\Message\ResponseInterface;
 use Psr\Http\Message\ServerRequestInterface;
 use Takemo101\Chubby\Http\Factory\SlimFactory;
 use Takemo101\CmsTool\Http\Routing\ThemeRouteGroupHandler;
+use Takemo101\CmsTool\Support\Accessor\ThemeCustomizationPreview;
+use Takemo101\CmsTool\UseCase\Theme\Support\ThemeCustomizationTemporaryCache;
 
 class ThemePreviewAction
 {
     /**
+     * constructor
+     *
+     * @param SlimFactory $factory
+     * @param RouteParserDecorator $routeParser
+     * @param ThemeCustomizationTemporaryCache $cache
+     * @param ActiveTheme $activeTheme
+     */
+    public function __construct(
+        private readonly SlimFactory $factory,
+        private readonly RouteParserDecorator $routeParser,
+        private readonly ThemeCustomizationTemporaryCache $cache,
+        private readonly ActiveTheme $activeTheme,
+    ) {
+        //
+    }
+
+    /**
      * Handle theme preview
      *
      * @param ServerRequestInterface $request
-     * @param SlimFactory $factory
-     * @param RouteParserDecorator $routeParser
+     * @param ThemeCustomizationPreview $preview
      * @param string|null $path
      * @return ResponseInterface
      */
     public function __invoke(
         ServerRequestInterface $request,
-        SlimFactory $factory,
-        RouteParserDecorator $routeParser,
+        ThemeCustomizationPreview $preview,
         ?string $path = null,
     ): ResponseInterface {
-        $slim = $factory->create();
+        $slim = $this->factory->create();
 
         $slim->setBasePath(
             $this->extractBasePath(
@@ -38,15 +56,22 @@ class ThemePreviewAction
 
         $slim->addRoutingMiddleware();
 
-        $routeParser->changeRouteParser(
+        $this->routeParser->changeRouteParser(
             $slim->getRouteCollector()->getRouteParser(),
         );
+
+        // If there is a cache, set it as preview data.
+        if ($data = $this->cache->get($this->activeTheme->id)) {
+            $preview->set(
+                $this->activeTheme->normalizeCustomization($data),
+            );
+        }
 
         return $slim->handle($request);
     }
 
     /**
-     * Extract base path from request
+     * Extract base path from request.
      *
      * @param ServerRequestInterface $request
      * @param string|null $suffixPath

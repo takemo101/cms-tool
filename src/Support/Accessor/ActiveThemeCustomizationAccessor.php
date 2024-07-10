d3efbfd7e9d6888eca1a1631692b03cbd82c7ff1
takemo101 <takemo101@gmail.com>
2024-07-11 07:53:52 +0900

    Refactor ActiveThemeCustomizationAccessor to use ThemeCustomizationPreview

--- src/Support/Accessor/ActiveThemeCustomizationAccessor.php
+++ src/Support/Accessor/ActiveThemeCustomizationAccessor.php
@@ -5,22 +5,19 @@ namespace Takemo101\CmsTool\Support\Accessor;
 use ArrayObject;
 use CmsTool\Theme\ActiveTheme;
 use CmsTool\Theme\Contract\ThemeCustomizationAccessor;
-use Takemo101\Chubby\Context\ContextRepository;
 use Takemo101\CmsTool\Support\Shared\ImmutableArrayObject;
 
 class ActiveThemeCustomizationAccessor
 {
-    public const ThemeCustomizationPreviewContextKey = 'theme.customization.preview';
-
     /**
      * constructor
      *
-     * @param ContextRepository $contextRepository
+     * @param ThemeCustomizationPreview $preview
      * @param ActiveTheme $activeTheme
      * @param ThemeCustomizationAccessor $accessor
      */
     public function __construct(
-        private readonly ContextRepository $contextRepository,
+        private readonly ThemeCustomizationPreview $preview,
         private readonly ActiveTheme $activeTheme,
         private readonly ThemeCustomizationAccessor $accessor,
     ) {
@@ -32,18 +29,13 @@ class ActiveThemeCustomizationAccessor
      */
     public function __invoke(): ArrayObject
     {
-        $context = $this->contextRepository->get();
-
-        // If the context has the theme customization data, use it.
-        if ($context->has(self::ThemeCustomizationPreviewContextKey)) {
-            /** @var array<string,array<string,mixed>> */
-            $data = $context->get(self::ThemeCustomizationPreviewContextKey, []);
-
-            return ImmutableArrayObject::of($data);
-        }
+        $data = $this->preview->get();
 
         return ImmutableArrayObject::of(
-            $this->accessor->load($this->activeTheme),
+            // If the preview data is set, return it.
+            $data !== false
+                ? $data
+                : $this->accessor->load($this->activeTheme),
         );
     }
 }

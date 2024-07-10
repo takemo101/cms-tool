37db719cec5b88d35152ca98a0647da8c5d501d3
takemo101 <takemo101@gmail.com>
2024-07-10 15:49:05 +0900

    Refactor ThemePreviewAction for improved theme routing

--- src/Http/Action/ThemePreviewAction.php
+++ src/Http/Action/ThemePreviewAction.php
@@ -14,24 +14,51 @@ class ThemePreviewAction
      *
      * @param ServerRequestInterface $request
      * @param SlimFactory $factory
-     * @param string $path
+     * @param string|null $path
      * @return ResponseInterface
      */
     public function __invoke(
         ServerRequestInterface $request,
         SlimFactory $factory,
-        string $path = '',
+        ?string $path = null,
     ): ResponseInterface {
         $slim = $factory->create();
 
+        $slim->setBasePath(
+            $this->extractBasePath(
+                request: $request,
+                suffixPath: $path,
+            ),
+        );
+
         ThemeRouteGroupHandler::configure($slim);
+
+        $slim->getRouteCollector()->getNamedRoute('home')->setPattern('');
 
         $slim->addRoutingMiddleware();
 
-        return $slim->handle(
-            $request->withUri(
-                $request->getUri()->withPath($path),
-            ),
-        );
+        return $slim->handle($request);
+    }
+
+    /**
+     * Extract base path from request
+     *
+     * @param ServerRequestInterface $request
+     * @param string|null $suffixPath
+     * @return string
+     */
+    private function extractBasePath(
+        ServerRequestInterface $request,
+        ?string $suffixPath = null,
+    ): string {
+        $basePath = $suffixPath
+            ? str_replace(
+                $suffixPath,
+                '',
+                $request->getUri()->getPath(),
+            )
+            : $request->getUri()->getPath();
+
+        return rtrim($basePath, '/');
     }
 }

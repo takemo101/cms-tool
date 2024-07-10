4bac891e610aebade6d13607d80c7a69c3fb78fa
takemo101 <takemo101@gmail.com>
2024-07-10 20:29:46 +0900

    feat: Add CacheThemeCustomizationHandler for temporarily caching theme customization data

---
+++ src/UseCase/Theme/Handler/CacheThemeCustomizationHandler.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Takemo101\CmsTool\UseCase\Theme\Handler;
+
+use CmsTool\Theme\Exception\NotFoundThemeException;
+use CmsTool\Theme\Theme;
+use CmsTool\Theme\ThemeId;
+use CmsTool\Theme\ThemeQueryService;
+use Takemo101\CmsTool\UseCase\Shared\Exception\NotFoundDataException;
+use Takemo101\CmsTool\UseCase\Theme\Support\ThemeCustomizationTemporaryCache;
+
+/**
+ * Save the theme customization data temporarily.
+ */
+class CacheThemeCustomizationHandler
+{
+    /**
+     * constructor
+     *
+     * @param ThemeQueryService $queryService
+     * @param ThemeCustomizationTemporaryCache $cache
+     */
+    public function __construct(
+        private readonly ThemeQueryService $queryService,
+        private readonly ThemeCustomizationTemporaryCache $cache,
+    ) {
+        //
+    }
+
+    /**
+     * Execute the processing the processing
+     *
+     * @param string $id
+     * @param array<string,array<string,mixed>> $data
+     * @return Theme
+     * @throws NotFoundDataException
+     */
+    public function handle(string $id, array $data): Theme
+    {
+        $themeId = new ThemeId($id);
+
+        try {
+            $theme = $this->queryService->getOne($themeId);
+        } catch (NotFoundThemeException $e) {
+            throw new NotFoundDataException(
+                message: $e->getMessage(),
+                previous: $e,
+            );
+        }
+
+        $extractedData = $theme->extractCustomizationData($data);
+
+        $this->cache->put(
+            id: $themeId,
+            data: $extractedData,
+        );
+
+        return $theme;
+    }
+}

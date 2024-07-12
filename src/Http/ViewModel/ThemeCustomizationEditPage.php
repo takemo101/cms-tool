266631b8a564fffdce202c9c0035b6562a57ba15
takemo101 <takemo101@gmail.com>
2024-07-13 08:39:16 +0900

    feat: Add ULID generation for setting UIDs in theme customization schema

--- src/Http/ViewModel/ThemeCustomizationEditPage.php
+++ src/Http/ViewModel/ThemeCustomizationEditPage.php
@@ -6,6 +6,7 @@ use ArrayObject;
 use CmsTool\Theme\Contract\ThemeCustomizationLoader;
 use CmsTool\Theme\Theme;
 use Takemo101\CmsTool\Support\Shared\ImmutableArrayObject;
+use Takemo101\CmsTool\Domain\Shared\IdCreator;
 
 class ThemeCustomizationEditPage extends ViewModel
 {
@@ -18,6 +19,32 @@ class ThemeCustomizationEditPage extends ViewModel
         public readonly Theme $theme,
     ) {
         //
+    }
+
+    /**
+     * Get theme schema.
+     *
+     * @return array<string,array<string,mixed>>
+     */
+    public function schema(): array
+    {
+        $schema = $this->theme->meta->schema->toArray();
+
+        $data = array_map(
+            fn (array $settings) => [
+                ...$settings,
+                'settings' => array_map(
+                    fn (array $setting) => [
+                        'uid' => IdCreator::ulid()->generate(),
+                        ...$setting,
+                    ],
+                    $settings['settings'],
+                ),
+            ],
+            $schema,
+        );
+
+        return $data;
     }
 
     /**

412c61548b89d1ca064ca6e43f2303d77387d70d
takemo101 <takemo101@gmail.com>
2024-07-31 21:03:10 +0900

    feat: Refactor HeaderTitle class to extend AbstractHeaderTitle

---
+++ src/Preset/Shared/HeaderTitle/LayeredHeaderTitle.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Takemo101\CmsTool\Preset\Shared\HeaderTitle;
+
+/**
+ * Data class for layered header titles of hierarchical content.
+ *
+ * @immutable
+ */
+readonly class LayeredHeaderTitle extends AbstractHeaderTitle
+{
+    //
+}

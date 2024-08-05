52f3934e2abe88e9c235104a55f53ad41733ca21
takemo101 <takemo101@gmail.com>
2024-08-05 13:23:54 +0900

    feat: Add ContentDetailHelper class for retrieving previous and next content

--- src/Preset/MicroCms/Shared/ViewModel/ContentDetailPage.php
+++ src/Preset/MicroCms/Shared/ViewModel/ContentDetailPage.php
@@ -3,6 +3,7 @@
 namespace Takemo101\CmsTool\Preset\MicroCms\Shared\ViewModel;
 
 use ArrayObject;
+use Closure;
 use Takemo101\CmsTool\Http\ViewModel\ViewModel;
 
 class ContentDetailPage extends ViewModel
@@ -11,12 +12,24 @@ class ContentDetailPage extends ViewModel
      * constructor
      *
      * @param ArrayObject $content
+     * @param Closure():array{0:?ArrayObject, 1:?ArrayObject} $prevAndNextContentsGenerator [0 => prev, 1 => next]
      * @param bool $isDraft
      */
     public function __construct(
-        public ArrayObject $content,
-        public bool $isDraft = false,
+        public readonly ArrayObject $content,
+        private readonly Closure $prevAndNextContentsGenerator,
+        public readonly bool $isDraft = false,
     ) {
         //
+    }
+
+    /**
+     * Get a closure to retrieve the next and previous content.
+     *
+     * @return ContentDetailHelper
+     */
+    public function helper(): ContentDetailHelper
+    {
+        return new ContentDetailHelper($this->prevAndNextContentsGenerator);
     }
 }

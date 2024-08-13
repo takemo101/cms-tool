dcce6c59bffc84ca5b57a4b35e0da4e4627ce44b
takemo101 <takemo101@gmail.com>
2024-08-13 11:17:30 +0900

    feat: htmlのhead部分への描画を管理するAbstractHtmlableCollection and HeadHtmlsクラスを追加

---
+++ src/Support/Htmlable/HeadHtmls.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Takemo101\CmsTool\Support\Htmlable;
+
+/**
+ * Class for managing the HTML output of the head tag.
+ */
+class HeadHtmls extends AbstractHtmlableCollection
+{
+    //
+}

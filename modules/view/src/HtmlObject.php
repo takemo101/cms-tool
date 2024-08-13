a357dd60c4cf44bc8cff5d11b3f7d425bafb2b72
takemo101 <takemo101@gmail.com>
2024-08-13 11:19:03 +0900

    fix: HtmlObjectの受け取る型を修正

--- modules/view/src/HtmlObject.php
+++ modules/view/src/HtmlObject.php
@@ -3,25 +3,37 @@
 namespace CmsTool\View;
 
 use CmsTool\View\Contract\Htmlable;
+use Stringable;
+use Takemo101\Chubby\Contract\Renderable;
 
 readonly class HtmlObject implements Htmlable
 {
     /**
      * constructor
      *
-     * @param string $html
+     * @param string|Stringable|Renderable $html
      */
     public function __construct(
-        private string $html,
+        private string|Stringable|Renderable $html,
     ) {
         //
     }
 
     /**
-     * @return string
+     * {@inheritDoc}
      */
     public function __toString(): string
     {
-        return $this->html;
+        $html = $this->html;
+
+        if ($html instanceof Stringable) {
+            return $html->__toString();
+        }
+
+        if ($html instanceof Renderable) {
+            return $html->render();
+        }
+
+        return $html;
     }
 }

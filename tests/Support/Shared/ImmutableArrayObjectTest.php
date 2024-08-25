2b2e77368ebd8dd25b12ea0bed11f100e03661c9
takemo101 <takemo101@gmail.com>
2024-08-25 15:58:59 +0900

    fix: Refactor array classes for improved immutability and search functionality

--- tests/Support/Shared/ImmutableArrayObjectTest.php
+++ tests/Support/Shared/ImmutableArrayObjectTest.php
@@ -1,53 +1,11 @@
 <?php
 
-use Takemo101\CmsTool\Support\Shared\ImmutableArrayObject;
+use Takemo101\CmsTool\Support\ArrayObject\ImmutableArrayObject;
 
 describe(
     'ImmutableArrayObject',
     function () {
-        it('should create an immutable array object', function () {
-            $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
-                'key4' => 'value4',
-                'key5' => [
-                    'key6' => 'value6',
-                ],
-                'key7' => [
-                    'data1',
-                    'data2',
-                    'data3',
-                ],
-            ]];
-
-            $immutableArray = ImmutableArrayObject::of($data);
-
-            expect($immutableArray->toArray())->toBe($data);
-            expect($immutableArray->key3->toArray())->toBe($data['key3']);
-            expect($immutableArray->key3->key4)->toBe($data['key3']['key4']);
-            expect($immutableArray->key3->key5['key6'])->toBe($data['key3']['key5']['key6']);
-            expect($immutableArray->key3->key7[0])->toBe($data['key3']['key7'][0]);
-        });
-
-        it('should throw an exception when trying to set a value', function () {
-            $immutableArray = ImmutableArrayObject::of();
-
-            expect(function () use ($immutableArray) {
-                $immutableArray->key = 'value';
-            })->toThrow(OutOfBoundsException::class);
-        });
-
-        it('should throw an exception when trying to unset a value', function () {
-            $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
-                'key4' => 'value4',
-                'key5' => 'value5',
-            ]];
-            $immutableArray = ImmutableArrayObject::of($data);
-
-            expect(function () use ($immutableArray) {
-                unset($immutableArray['key1']);
-            })->toThrow(OutOfBoundsException::class);
-        });
-
-        it('should get the value of a key from the immutable array object', function () {
+        it('should get the value of a key from the immutable array object using offsetGet', function () {
             $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
                 'key4' => 'value4',
                 'key5' => [
@@ -68,7 +26,7 @@ describe(
             expect($immutableArray->get('key3.key7.0'))->toBe($data['key3']['key7'][0]);
         });
 
-        it('should get the default value when the key does not exist in the immutable array object', function () {
+        it('should check if a key exists in the immutable array object using offsetExists', function () {
             $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
                 'key4' => 'value4',
                 'key5' => [
@@ -83,10 +41,117 @@ describe(
 
             $immutableArray = ImmutableArrayObject::of($data);
 
-            expect($immutableArray->get('key8', 'default'))->toBe('default');
-            expect($immutableArray->get('key3.key8', 'default'))->toBe('default');
-            expect($immutableArray->get('key3.key5.key8', 'default'))->toBe('default');
-            expect($immutableArray->get('key3.key7.3', 'default'))->toBe('default');
+            expect(isset($immutableArray['key1']))->toBe(true);
+            expect(isset($immutableArray['key3']['key4']))->toBe(true);
+            expect(isset($immutableArray['key3']['key5']['key6']))->toBe(true);
+            expect(isset($immutableArray['key3']['key7'][0]))->toBe(true);
+            expect(isset($immutableArray['nonexistent']))->toBe(false);
+        });
+
+        it('should get the default value when the key does not exist in the immutable array object using offsetGet', function () {
+            $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
+                'key4' => 'value4',
+                'key5' => [
+                    'key6' => 'value6',
+                ],
+                'key7' => [
+                    'data1',
+                    'data2',
+                    'data3',
+                ],
+            ]];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray['nonexistent'])->toBe(null);
+            expect($immutableArray['key3.nonexistent'])->toBe(null);
+            expect($immutableArray['key3.key5.nonexistent'])->toBe(null);
+            expect($immutableArray['key3.key7.3'])->toBe(null);
+        });
+
+        it('should throw an exception when trying to set a value using offsetSet', function () {
+            $immutableArray = ImmutableArrayObject::of();
+
+            expect(function () use ($immutableArray) {
+                $immutableArray['key'] = 'value';
+            })->toThrow(OutOfBoundsException::class);
+        });
+
+        it('should throw an exception when trying to unset a value using offsetUnset', function () {
+            $data = ['key1' => 'value1', 'key2' => 'value2', 'key3' => [
+                'key4' => 'value4',
+                'key5' => 'value5',
+            ]];
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect(function () use ($immutableArray) {
+                unset($immutableArray['key1']);
+            })->toThrow(OutOfBoundsException::class);
+        });
+
+        it('should get the value of a snake case key from the immutable array object using offsetGet', function () {
+            $data = [
+                'allOf' => 'value',
+                'keyOf' => 'value',
+            ];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray->all_of)->toBe($data['allOf']);
+            expect($immutableArray['all_of'])->toBe($data['allOf']);
+            expect($immutableArray->key_of)->toBe($data['keyOf']);
+            expect($immutableArray['key_of'])->toBe($data['keyOf']);
+        });
+
+        it('should get the value of a key from the immutable array object using toArray', function () {
+            $data = [
+                'key1' => 'value1',
+                'key2' => 'value2',
+            ];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray->toArray())->toBe($data);
+
+            $data = [
+                'key1' => 'value1',
+                'key2' => ImmutableArrayObject::of([
+                    'key3' => 'value3',
+                ]),
+            ];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray->toArray())->toBe([
+                ...$data,
+                'key2' => $data['key2']->toArray(),
+            ]);
+        });
+
+
+        it('should get the value of a key from the immutable array object using jsonSerialize', function () {
+            $data = [
+                'key1' => 'value1',
+                'key2' => 'value2',
+            ];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray->jsonSerialize())->toBe($data);
+
+            $data = [
+                'key1' => 'value1',
+                'key2' => ImmutableArrayObject::of([
+                    'key3' => 'value3',
+                ]),
+            ];
+
+            $immutableArray = ImmutableArrayObject::of($data);
+
+            expect($immutableArray->jsonSerialize())->toBe([
+                ...$data,
+                'key2' => $data['key2']->jsonSerialize(),
+            ]);
         });
     }
 )->group('ImmutableArrayObject', 'support');

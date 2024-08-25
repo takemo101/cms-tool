23ab9d004beffdd3f6183e2c1b46e5f8a9256ca7
takemo101 <takemo101@gmail.com>
2024-08-25 18:29:28 +0900

    feat: Refactor property access methods to use camel case in HasCamelCasePropertyAccess trait

--- src/Support/Shared/HasCamelCasePropertyAccess.php
+++ src/Support/Shared/HasCamelCasePropertyAccess.php
@@ -9,6 +9,11 @@ use function Symfony\Component\String\u;
 trait HasCamelCasePropertyAccess
 {
     /**
+     * @var string[]
+     */
+    public const IgnorePropertyAccessMethods = [];
+
+    /**
      * Get the value corresponding to the property name.
      *
      * @return array<string,mixed> [propertyNames => propertyValues]
@@ -60,6 +65,24 @@ trait HasCamelCasePropertyAccess
 
         if (property_exists($this, $camel)) {
             return $this->{$camel};
+        }
+
+        // If there is a method with the same name as the property, call it.
+        if (!in_array($name, [
+            ...self::IgnorePropertyAccessMethods,
+            '__construct',
+            '__get',
+        ])) {
+            $method = method_exists($this, $name)
+                ? $name : (
+                    method_exists($this, $camel)
+                    ? $camel
+                    : false
+                );
+
+            if ($method) {
+                return $this->{$method}();
+            }
         }
 
         // __properties() is a method that returns an array of properties.

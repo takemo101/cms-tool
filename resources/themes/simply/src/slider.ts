677e104fa7db34942110488b42e3ec72e6295de0
takemo101 <takemo101@gmail.com>
2024-08-27 21:54:38 +0900

    fix: 表示パフォーマンスを改善するためにファイルを分割してビルドするようにした

--- resources/themes/simply/src/slider.ts
+++ resources/themes/simply/src/slider.ts
@@ -1,8 +1,7 @@
-import "@splidejs/splide/css";
 import Splide from "@splidejs/splide";
 import type { Options, Splide as SplideType } from "@splidejs/splide";
 
-export default class Slider {
+class Slider {
   public mount(target: string | HTMLElement, options: Options = {}): SplideType {
     return new Splide(target, {
       autoplay: true,
@@ -11,3 +10,5 @@ export default class Slider {
     }).mount();
   }
 }
+
+window.Slider = new Slider();
--- resources/themes/simply/vite.config.ts
+++ resources/themes/simply/vite.config.ts
@@ -10,10 +10,12 @@ export default defineConfig(({ command }) =>
         rollupOptions: {
           input: {
             app: resolve(__dirname, "src", "app.ts"),
+            slider: resolve(__dirname, "src", "slider.ts"),
+            highlight: resolve(__dirname, "src", "highlight.ts"),
           },
           output: {
             entryFileNames: "[name].js",
-            chunkFileNames: "[name].js",
+            chunkFileNames: "vendor/[name].js",
             assetFileNames: "[name].[ext]",
           },
         },

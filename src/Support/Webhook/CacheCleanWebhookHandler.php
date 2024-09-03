35eef64f0c5f9128c3839d1f0b5d1dba78714a33
takemo101 <takemo101@gmail.com>
2024-09-03 13:12:53 +0900

    refactor: ウェブフック処理にApiMemoCacheのキャッシュクリア処理を追加

--- src/Support/Webhook/CacheCleanWebhookHandler.php
+++ src/Support/Webhook/CacheCleanWebhookHandler.php
@@ -3,6 +3,7 @@
 namespace Takemo101\CmsTool\Support\Webhook;
 
 use Psr\Cache\CacheItemPoolInterface;
+use Takemo101\CmsTool\Infra\Cache\ApiMemoCache;
 
 class CacheCleanWebhookHandler implements WebhookHandler
 {
@@ -10,9 +11,11 @@ class CacheCleanWebhookHandler implements WebhookHandler
      * constructor
      *
      * @param CacheItemPoolInterface $cache
+     * @param ApiMemoCache $memo
      */
     public function __construct(
         private CacheItemPoolInterface $cache,
+        private ApiMemoCache $memo,
     ) {
         //
     }
@@ -23,5 +26,6 @@ class CacheCleanWebhookHandler implements WebhookHandler
     public function handle(array $payload): void
     {
         $this->cache->clear();
+        $this->memo->clear();
     }
 }
